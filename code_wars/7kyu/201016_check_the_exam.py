12c8dc2247c881de479c64dbd9d5cd50b6b2e3b9
TaeHyoungKwon <kth5604@gmail.com>
2020-11-27 10:07:49 +0900

    201127

--- code_wars/7kyu/201016_check_the_exam.py
+++ code_wars/7kyu/201016_check_the_exam.py
@@ -5,18 +5,20 @@ def check_exam(arr1, arr2):
     exam_result = {
         'o': 0,
         'x': 0,
-        'blank': 0,
     }
 
     for index, ele in enumerate(arr2):
+        if ele == '':
+            continue
+
         if ele == arr1[index]:
             exam_result['o'] += 4
-        elif ele == '':
-            return exam_result['o'] + exam_result['x']
-        elif ele != arr1[index]:
+        else:
             exam_result['x'] -= 1
 
-    return exam_result['o'] + exam_result['x']
+    total = exam_result['o'] + exam_result['x']
+
+    return total if total > 0 else 0
 
 
 class TestCheckExam(unittest.TestCase):
@@ -24,3 +26,8 @@ class TestCheckExam(unittest.TestCase):
         arr1, arr2 = ["a", "a", "b", "b"], ["a", "c", "b", "d"]
         actual = check_exam(arr1, arr2)
         self.assertEqual(actual, 6)
+
+    def test_check_exam_with_edge_case(self):
+        arr1, arr2 = ["a", "a", "c", "b"], ["a", "a", "b", ""]
+        actual = check_exam(arr1, arr2)
+        self.assertEqual(actual, 7)
