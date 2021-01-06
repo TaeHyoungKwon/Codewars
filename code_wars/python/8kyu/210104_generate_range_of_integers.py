33f114b5fb4c6c3f08e8e283b41f258c85c359e5
TaeHyoungKwon <kth5604@gmail.com>
2021-01-06 23:08:42 +0900

    210105

--- code_wars/python/8kyu/210104_generate_range_of_integers.py
+++ code_wars/python/8kyu/210104_generate_range_of_integers.py
@@ -8,3 +8,6 @@ def generate_range(min_, max_, step_):
 class TestGenerateRange(unittest.TestCase):
     def test_generate_range(self):
         self.assertEqual(generate_range(2, 10, 2), [2, 4, 6, 8, 10])
+
+    def test_generate_range_with_edge_case(self):
+        self.assertEqual(generate_range(2, 10, 3), [2, 5, 8])
