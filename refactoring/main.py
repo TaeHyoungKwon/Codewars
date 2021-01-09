3c693852ad6ce1cf68582cc110e024fee9a89c2a
TaeHyoungKwon <kth5604@gmail.com>
2021-01-09 21:40:40 +0900

    refactoring: extract inner method about total_amounts and total_volume_credits

--- refactoring/main.py
+++ refactoring/main.py
@@ -3,14 +3,13 @@ import math
 
 
 def statement(invoice, plays):
-
     def amount_for(a_performance):
         result = 0
-        if play_for(perf)["type"] == "tragedy":
+        if play_for(a_performance)["type"] == "tragedy":
             result = 40000
             if a_performance["audience"] > 30:
                 result += 1000 * (a_performance["audience"] - 30)
-        elif play_for(perf)["type"] == "comedy":
+        elif play_for(a_performance)["type"] == "comedy":
             result = 30000
             if a_performance["audience"] > 20:
                 result += 10000 + 500 * (a_performance["audience"] - 20)
@@ -30,7 +29,7 @@ def statement(invoice, plays):
         return result
 
     def usd(a_number):
-        return '${:,.2f}'.format(a_number / 100)
+        return "${:,.2f}".format(a_number / 100)
 
     def total_volume_credits():
         result = 0
@@ -38,25 +37,27 @@ def statement(invoice, plays):
             result += volume_credits_for(perf)
         return result
 
-    total_amount = 0
+    def total_amounts():
+        result = 0
+        for perf in invoice["performances"]:
+            result += amount_for(perf)
+        return result
+
     result = f"청구 내역 (고객명: {invoice['customer']})\n"
-
-    for perf in invoice["performances"]:
-        result += f' {play_for(perf)["name"]}: {usd(amount_for(perf))} ({perf["audience"]}석)\n'
-        total_amount += amount_for(perf)
-
-    result += f"총액: {usd(total_amount)}\n"
+    result += "".join(
+        f' {play_for(perf)["name"]}: {usd(amount_for(perf))} ({perf["audience"]}석)\n'
+        for perf in invoice["performances"]
+    )
+    result += f"총액: {usd(total_amounts())}\n"
     result += f"적립 포인트': {total_volume_credits()}점\n"
     return result
 
 
-if __name__ == '__main__':
-    with open('invoices.json') as json_file:
+if __name__ == "__main__":
+    with open("invoices.json") as json_file:
         invoice = json.load(json_file)[0]
 
-    with open('plays.json') as json_file:
+    with open("plays.json") as json_file:
         plays = json.load(json_file)
 
     print(statement(invoice, plays))
-
-
