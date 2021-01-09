42238e7db56607811229b3df18ef64553d0d5acb
TaeHyoungKwon <kth5604@gmail.com>
2021-01-09 20:12:57 +0900

    refactoring: delete variable in parameter

--- refactoring/main.py
+++ refactoring/main.py
@@ -4,13 +4,13 @@ import math
 
 def statement(invoice, plays):
 
-    def amount_for(a_performance, play):
+    def amount_for(a_performance):
         result = 0
-        if play["type"] == "tragedy":
+        if play_for(perf)["type"] == "tragedy":
             result = 40000
             if a_performance["audience"] > 30:
                 result += 1000 * (a_performance["audience"] - 30)
-        elif play["type"] == "comedy":
+        elif play_for(perf)["type"] == "comedy":
             result = 30000
             if a_performance["audience"] > 20:
                 result += 10000 + 500 * (a_performance["audience"] - 20)
@@ -19,20 +19,22 @@ def statement(invoice, plays):
             raise Exception("알 수 없는 장르")
         return result
 
+    def play_for(a_performance):
+        return plays[a_performance["playID"]]
+
     total_amount = 0
     volume_credits = 0
     result = f"청구 내역 (고객명: {invoice['customer']})\n"
     dollar_format = '${:,.2f}'
 
     for perf in invoice["performances"]:
-        play = plays[perf["playID"]]
-        this_amount = amount_for(perf, play)
+        this_amount = amount_for(perf)
         volume_credits += max(perf["audience"] - 30, 0)
 
-        if play["type"] == "comedy":
+        if play_for(perf)["type"] == "comedy":
             volume_credits += math.floor(perf["audience"] / 5)
 
-        result += f' {play["name"]}: {dollar_format.format(this_amount / 100)} ({perf["audience"]}석)\n'
+        result += f' {play_for(perf)["name"]}: {dollar_format.format(this_amount / 100)} ({perf["audience"]}석)\n'
         total_amount += this_amount
 
     result += f"총액: {dollar_format.format(total_amount / 100)}\n"
