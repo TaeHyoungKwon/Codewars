8dba3b9b74b50edf1a3bbd2353cbabc3aa86d219
TaeHyoungKwon <kth5604@gmail.com>
2021-06-27 18:04:57 +0900

    210627

--- c_2021/code_spitz/no_1/homework.py
+++ c_2021/code_spitz/no_1/homework.py
@@ -1,23 +1,34 @@
 import unittest
 
 
-def sum_arr(l):
-    return sum(l)
+def sum_arr(num_list):
+    return sum(num_list)
+
+
+def sum_arr_with_loop(num_list):
+    acc = 0
+    for i in range(1, len(num_list) + 1):
+        acc += i
+    return acc
 
 
 def sum_arr_with_recursive(l, n):
     return n + sum_arr_with_recursive(l[:n], n - 1) if n > 0 else 0
 
-def test_sum_arr_with_tail_recursive(l, n):
-    return
+
+def sum_arr_with_tail_recursive(l, n, acc=0):
+    return sum_arr_with_tail_recursive(l[:n], n - 1, acc + n) if n > 0 else acc + n
 
 
 class TestSumArr(unittest.TestCase):
     def test_sum_arr(self):
-        self.assertEqual(sum_arr(l=[1, 2, 3, 4, 5]), 15)
+        self.assertEqual(sum_arr(num_list=[1, 2, 3, 4, 5]), 15)
+
+    def test_sum_arr_with_loop(self):
+        self.assertEqual(sum_arr_with_loop(num_list=[1, 2, 3, 4, 5]), 15)
 
     def test_sum_arr_with_recursive(self):
         self.assertEqual(sum_arr_with_recursive(l=[1, 2, 3, 4, 5], n=5), 15)
 
     def test_sum_arr_with_tail_recursive(self):
-        self.assertEqual(test_sum_arr_with_tail_recursive(l=[1, 2, 3, 4, 5], n=5), 15)
+        self.assertEqual(sum_arr_with_tail_recursive(l=[1, 2, 3, 4, 5], n=5), 15)
