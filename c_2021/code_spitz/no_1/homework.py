2d8bee661ada5c04dda6d7978a33cde85ac6fc63
TaeHyoungKwon <kth5604@gmail.com>
2021-06-28 21:59:51 +0900

    210628

--- c_2021/code_spitz/no_1/homework.py
+++ c_2021/code_spitz/no_1/homework.py
@@ -16,8 +16,12 @@ def sum_arr_with_recursive(l, n):
     return n + sum_arr_with_recursive(l[:n], n - 1) if n > 0 else 0
 
 
-def sum_arr_with_tail_recursive(l, n, acc=0):
-    return sum_arr_with_tail_recursive(l[:n], n - 1, acc + n) if n > 0 else acc + n
+# def sum_arr_with_tail_recursive(l, n, acc=0):
+#     return sum_arr_with_tail_recursive(l[:n], n - 1, acc + n) if n > 0 else acc + n
+
+
+def sum_arr_with_tail_recursive(l, i, acc=0):
+    return sum_arr_with_tail_recursive(l, i - 1, acc + l[i - 1]) if i > 0 else acc + i
 
 
 class TestSumArr(unittest.TestCase):
@@ -31,4 +35,4 @@ class TestSumArr(unittest.TestCase):
         self.assertEqual(sum_arr_with_recursive(l=[1, 2, 3, 4, 5], n=5), 15)
 
     def test_sum_arr_with_tail_recursive(self):
-        self.assertEqual(sum_arr_with_tail_recursive(l=[1, 2, 3, 4, 5], n=5), 15)
+        self.assertEqual(sum_arr_with_tail_recursive(l=[1, 2, 3, 4, 5], i=5), 15)
