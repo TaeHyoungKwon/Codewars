d201a88939dec2c935f3bd97af27152e3e8a5ed7
TaeHyoungKwon <kth5604@gmail.com>
2021-07-01 09:06:14 +0900

    210701 add pydantic cheat sheet

--- c_2021/pydantic_test/cheat_sheet/basic_model_usage.py
+++ c_2021/pydantic_test/cheat_sheet/basic_model_usage.py
@@ -11,6 +11,7 @@ class User(BaseModel):
 user = User(id="123")
 
 if __name__ == "__main__":
+    # Basic model usage
     assert user.id == 123
     assert user.name == "Jane Doe"
     assert user.dict() == dict(user) == {"id": 123, "name": "Jane Doe"}
@@ -19,3 +20,8 @@ if __name__ == "__main__":
     assert user.copy().id == user.id  # shallow copy
     assert User.parse_obj({"id": 123, "name": "Jane Doe"}) == user
     assert User.parse_raw(user.json()) == user
+    assert User.parse_file("test.json") == user
+    assert json.dumps(User.schema()) == User.schema_json()  # User 모델의 상세 정보를 json으로 리턴
+    assert User.construct(id="abcde", name=123).dict() == {"id": "abcde", "name": 123}  # type hint validation을 무시할 수 있음
+
+
---
+++ c_2021/pydantic_test/cheat_sheet/recursive_models.py
@@ -0,0 +1,30 @@
+from typing import List
+from pydantic import BaseModel
+
+
+class Foo(BaseModel):
+    count: int
+    size: float = None
+
+
+class Bar(BaseModel):
+    apple = "x"
+    banana = "y"
+
+
+class Spam(BaseModel):
+    foo: Foo
+    bars: List[Bar]
+
+
+if __name__ == "__main__":
+    # Recursive Models
+    m = Spam(foo={"count": 4}, bars=[{"apple": "x1"}, {"apple": "x2"}])
+    m.dict() == {
+        "foo": {"count": 4, "size": None},
+        "bars": [
+            {"apple": "x1", "banana": "y"},
+            {"apple": "x2", "banana": "y"},
+        ],
+    }
+
