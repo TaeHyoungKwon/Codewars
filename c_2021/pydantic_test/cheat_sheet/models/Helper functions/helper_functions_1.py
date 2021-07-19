dc4c014c14123be8e2f7472e85aefafaee26dd9f
TaeHyoungKwon <kth5604@gmail.com>
2021-07-19 13:44:13 +0900

    210719

---
+++ c_2021/pydantic_test/cheat_sheet/models/Helper functions/helper_functions_1.py
@@ -0,0 +1,38 @@
+import pickle
+from datetime import datetime
+
+from pydantic import BaseModel, ValidationError
+
+
+class User(BaseModel):
+    id: int
+    name = "John Doe"
+    signup_ts: datetime = None
+
+
+if __name__ == "__main__":
+    """
+    parse_obj
+    * __init__ 메소드와 비슷한 역할을 한다
+    * 다른 점은 오직 dict로 만 넘겨야 하고, 그렇지 않으면 ValidationError를 일으킨다
+    """
+    m = User.parse_obj({"id": 123, "name": "James"})
+    print(m)
+    # id=123 signup_ts=None name='James'
+
+    try:
+        User.parse_obj(["not", "a", "dict"])
+    except ValidationError as e:
+        print(e)
+        """
+        1 validation error for User
+        __root__
+        User expected dict not list (type=type_error)
+        """
+
+    m = User.parse_raw('{"id": 123, "name": "James"}')
+    print(m)
+    # id=123 signup_ts=None name='James'
+    """
+    parse_raw 작성 해야함
+    """
