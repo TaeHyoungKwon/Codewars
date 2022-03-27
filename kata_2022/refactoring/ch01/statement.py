d318821faa7cc197500d6f45f0e3f04e7bea6830
TaeHyoungKwon <kth5604@gmail.com>
2022-03-28 01:56:50 +0900

    [refactoring-ch01] extract get_volume_credits_for

--- kata_2022/refactoring/ch01/statement.py
+++ kata_2022/refactoring/ch01/statement.py
@@ -4,14 +4,11 @@ from typing import Any, Union
 
 def statement(invoice: dict[str, Any], plays: dict[str, Any]) -> str:
     total_amount = 0
-    volume_credits = 0
     result = f'Invoice (Customer: {invoice["customer"]})\n'
     dollar_format = "${:,.2f}".format
 
     for performance in invoice["performances"]:
-        volume_credits += max(performance["audience"] - 30, 0)
-        if get_play_for(performance, plays)["type"] == "comedy":
-            volume_credits += math.floor(performance["audience"] / 5)
+        volume_credits = get_volume_credits_for(performance, plays)
 
         result += f'\t{get_play_for(performance, plays)["name"]}: {dollar_format(get_amount_for(performance, plays) / 100)} ({performance["audience"]} Seats)\n'
         total_amount += get_amount_for(performance, plays)
@@ -19,6 +16,14 @@ def statement(invoice: dict[str, Any], plays: dict[str, Any]) -> str:
     result += f"Total Amount: {dollar_format(total_amount / 100)}\n"
     result += f"Volume Credits: {volume_credits}\n"
     return result
+
+
+def get_volume_credits_for(performance, plays):
+    volume_credits = 0
+    volume_credits += max(performance["audience"] - 30, 0)
+    if get_play_for(performance, plays)["type"] == "comedy":
+        volume_credits += math.floor(performance["audience"] / 5)
+    return volume_credits
 
 
 def get_play_for(performance: dict[str, Union[str, int]], plays: dict) -> dict:
