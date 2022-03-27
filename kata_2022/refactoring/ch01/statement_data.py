990124bd6ddc9059004b3385377d51b0197c2da9
TaeHyoungKwon <kth5604@gmail.com>
2022-03-28 03:17:27 +0900

    [refactoring-ch01] create a file for the performance calculator

--- kata_2022/refactoring/ch01/statement_data.py
+++ kata_2022/refactoring/ch01/statement_data.py
@@ -3,17 +3,7 @@ from functools import reduce
 import math
 from typing import Union
 
-
-class PerformanceCalculator:
-    def __init__(self, performance: dict, play: dict):
-        self.performance = performance
-        self.play = play
-
-    def get_amount(self) -> int:
-        raise Exception("Use subclass")
-
-    def get_volume_credits(self) -> int:
-        return max(self.performance["audience"] - 30, 0)
+from kata_2022.refactoring.ch01.performance_calculator import create_performance_calculator
 
 
 def create_statement_data(invoice: dict, plays: dict) -> dict:
@@ -31,37 +21,6 @@ def enrich_performance(performance: dict, plays: dict) -> dict:
     result["play"] = calculator.play
     result["amount"] = calculator.get_amount()
     return result
-
-
-class TragedyCalculator(PerformanceCalculator):
-    def get_amount(self) -> int:
-        result = 40000
-        if self.performance["audience"] > 30:
-            result += 1000 * (self.performance["audience"] - 30)
-        return result
-
-    def get_volume_credits(self) -> int:
-        result = super().get_volume_credits()
-        result += math.floor(self.performance["audience"] / 5)
-        return result
-
-
-class ComedyCalculator(PerformanceCalculator):
-    def get_amount(self) -> int:
-        result = 30000
-        if self.performance["audience"] > 20:
-            result += 10000 + 500 * (self.performance["audience"] - 20)
-        result += 300 * self.performance["audience"]
-        return result
-
-
-def create_performance_calculator(performance: dict, play: dict) -> PerformanceCalculator:
-    if play["type"] == "tragedy":
-        return TragedyCalculator(performance, play)
-    elif play["type"] == "comedy":
-        return ComedyCalculator(performance, play)
-    else:
-        raise ValueError(f"Unknown genre: {play['type']}")
 
 
 def get_play_for(performance: dict[str, Union[str, int]], plays: dict) -> dict:
