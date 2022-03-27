c18adc040be0bdb8fc4cdfbaf5bd94d58e9192fc
TaeHyoungKwon <kth5604@gmail.com>
2022-03-28 02:03:11 +0900

    [refactoring-ch01] extract dollar_format function

--- kata_2022/refactoring/ch01/statement.py
+++ kata_2022/refactoring/ch01/statement.py
@@ -6,7 +6,6 @@ def statement(invoice: dict[str, Any], plays: dict[str, Any]) -> str:
     total_amount = 0
     volume_credits = 0
     result = f'Invoice (Customer: {invoice["customer"]})\n'
-    dollar_format = "${:,.2f}".format
 
     for performance in invoice["performances"]:
         volume_credits += get_volume_credits_for(performance, plays)
@@ -17,6 +16,10 @@ def statement(invoice: dict[str, Any], plays: dict[str, Any]) -> str:
     result += f"Total Amount: {dollar_format(total_amount / 100)}\n"
     result += f"Volume Credits: {volume_credits}\n"
     return result
+
+
+def dollar_format(num: float) -> str:
+    return "${:,.2f}".format(num)
 
 
 def get_volume_credits_for(performance: dict[str, Union[str, int]], plays: dict) -> int:
