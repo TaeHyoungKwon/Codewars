6cc2de9ba290777c7b9dc47c331fab35313c80f5
TaeHyoungKwon <kth5604@gmail.com>
2022-03-28 02:08:51 +0900

    [refactoring-ch01] split the for loop

--- kata_2022/refactoring/ch01/statement.py
+++ kata_2022/refactoring/ch01/statement.py
@@ -8,6 +8,8 @@ def statement(invoice: dict[str, Any], plays: dict[str, Any]) -> str:
 
     for performance in invoice["performances"]:
         result += f'\t{get_play_for(performance, plays)["name"]}: {dollar_format(get_amount_for(performance, plays) / 100)} ({performance["audience"]} Seats)\n'
+
+    for performance in invoice["performances"]:
         total_amount += get_amount_for(performance, plays)
 
     result += f"Total Amount: {dollar_format(total_amount / 100)}\n"
