69dfff3a6b646a9c6ebcba173e1ba6d248a3d97e
TaeHyoungKwon <kth5604@gmail.com>
2022-03-28 02:33:25 +0900

    [refactoring-ch01] change the function declarations

--- kata_2022/refactoring/ch01/statement.py
+++ kata_2022/refactoring/ch01/statement.py
@@ -7,7 +7,7 @@ def statement(invoice: dict[str, Any], plays: dict[str, Any]) -> str:
     statement_data = {}
     statement_data["customer"] = invoice["customer"]
     statement_data["performances"] = [enrich_performance(performance, plays) for performance in invoice["performances"]]
-    return render_plain_text(statement_data, plays)
+    return render_plain_text(statement_data)
 
 
 def enrich_performance(performance: dict, plays: dict) -> dict:
@@ -20,51 +20,55 @@ def get_play_for(performance: dict[str, Union[str, int]], plays: dict) -> dict:
     return plays[performance["playID"]]
 
 
-def render_plain_text(data: dict, plays: dict) -> str:
+def render_plain_text(data: dict) -> str:
     result = f'Invoice (Customer: {data["customer"]})\n'
     for performance in data["performances"]:
-        result += f'\t{get_play_for(performance, plays)["name"]}: {dollar_format(get_amount_for(performance, plays) / 100)} ({performance["audience"]} Seats)\n'
-    result += f"Total Amount: {dollar_format(get_total_amount(data, plays) / 100)}\n"
-    result += f"Volume Credits: {get_total_volume_credits(data, plays)}\n"
+        result += (
+            f'\t{performance["play"]["name"]}: '
+            f"{get_usd(get_amount_for(performance) / 100)} "
+            f'({performance["audience"]} Seats)\n'
+        )
+    result += f"Total Amount: {get_usd(get_total_amount(data) / 100)}\n"
+    result += f"Volume Credits: {get_total_volume_credits(data)}\n"
     return result
 
 
-def get_total_amount(data: dict, plays: dict) -> int:
+def get_total_amount(data: dict) -> int:
     result = 0
     for performance in data["performances"]:
-        result += get_amount_for(performance, plays)
+        result += get_amount_for(performance)
     return result
 
 
-def get_total_volume_credits(data: dict, plays: dict) -> int:
+def get_total_volume_credits(data: dict) -> int:
     result = 0
     for performance in data["performances"]:
-        result += get_volume_credits_for(performance, plays)
+        result += get_volume_credits_for(performance)
     return result
 
 
-def dollar_format(num: float) -> str:
+def get_usd(num: float) -> str:
     return "${:,.2f}".format(num)
 
 
-def get_volume_credits_for(performance: dict[str, Union[str, int]], plays: dict) -> int:
+def get_volume_credits_for(performance: dict[str, Union[str, int]]) -> int:
     result = max(performance["audience"] - 30, 0)
-    if get_play_for(performance, plays)["type"] == "comedy":
+    if performance["play"]["type"] == "comedy":
         result += math.floor(performance["audience"] / 5)
     return result
 
 
-def get_amount_for(performance: dict[str, Union[str, int]], plays: dict) -> int:
+def get_amount_for(performance: dict[str, Union[str, int]]) -> int:
     result = 0
-    if get_play_for(performance, plays)["type"] == "tragedy":
+    if performance["play"]["type"] == "tragedy":
         result = 40000
         if performance["audience"] > 30:
             result += 1000 * (performance["audience"] - 30)
-    elif get_play_for(performance, plays)["type"] == "comedy":
+    elif performance["play"]["type"] == "comedy":
         result = 30000
         if performance["audience"] > 20:
             result += 10000 + 500 * (performance["audience"] - 20)
         result += 300 * performance["audience"]
     else:
-        raise ValueError(f'Unknown genre: {get_play_for(performance, plays)["type"]}')
+        raise ValueError(f'Unknown genre: {performance["play"]["type"]}')
     return result
