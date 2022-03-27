2d049552cca43ba7c9f558d111eeb1eb6b183248
TaeHyoungKwon <kth5604@gmail.com>
2022-03-28 01:52:15 +0900

    [refactoring-ch01] change the declaration of get_amount_for

--- kata_2022/refactoring/ch01/statement.py
+++ kata_2022/refactoring/ch01/statement.py
@@ -9,7 +9,7 @@ def statement(invoice: dict[str, Any], plays: dict[str, Any]) -> str:
     dollar_format = "${:,.2f}".format
 
     for performance in invoice["performances"]:
-        this_amount = get_amount_for(performance, get_play_for(performance, plays))
+        this_amount = get_amount_for(performance, plays)
 
         volume_credits += max(performance["audience"] - 30, 0)
         if get_play_for(performance, plays)["type"] == "comedy":
@@ -27,17 +27,17 @@ def get_play_for(performance: dict[str, Union[str, int]], plays: dict) -> dict:
     return plays[performance["playID"]]
 
 
-def get_amount_for(performance: dict[str, Union[str, int]], play: dict) -> int:
+def get_amount_for(performance: dict[str, Union[str, int]], plays: dict) -> int:
     result = 0
-    if play["type"] == "tragedy":
+    if get_play_for(performance, plays)["type"] == "tragedy":
         result = 40000
         if performance["audience"] > 30:
             result += 1000 * (performance["audience"] - 30)
-    elif play["type"] == "comedy":
+    elif get_play_for(performance, plays)["type"] == "comedy":
         result = 30000
         if performance["audience"] > 20:
             result += 10000 + 500 * (performance["audience"] - 20)
         result += 300 * performance["audience"]
     else:
-        raise ValueError(f'Unknown genre: {play["type"]}')
+        raise ValueError(f'Unknown genre: {get_play_for(performance, plays)["type"]}')
     return result
