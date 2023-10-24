63b5a0bd8d88eb8243443f022e3edf54eb544496
TaeHyoungKwon <kth5604@gmail.com>
2023-10-24 09:51:19 +0900

    231024

--- Baekjoon Online Judge/탁구_경기.py
+++ Baekjoon Online Judge/탁구_경기.py
@@ -1,17 +1,16 @@
 def solution(winner_results: list[str]) -> str:
-    
     dalgu_win_count = 0
     ponix_win_count = 0
-    
+
     for winner in winner_results:
         if winner == "D":
             dalgu_win_count += 1
         else:
             ponix_win_count += 1
-            
+
         if abs(dalgu_win_count - ponix_win_count) == 2:
             break
-        
+
     return f"{dalgu_win_count}:{ponix_win_count}"
 
 
